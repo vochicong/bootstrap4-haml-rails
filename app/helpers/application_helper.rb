2c131e470751017f49cf000d9ba029d26a0df18c
Cong <kongvc@gmail.com>
2017-05-04 05:46:49 +0000

    Add links to source code on GitHub #1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def oss_url(file)
+    github_url(file)
+  end
+
+  def github_url(file)
+    "https://github.com/vochicong/bootstrap4-haml-rails/blob/master/" + file
+  end
 end
